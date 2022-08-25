57bc06ebfd15b96cf4ece010e7448c3cae1b3f12
JavierBarker <hernandez.javier2003@gmail.com>
2022-08-25 16:10:53 -0600

    Add functions to app

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder='Escriba su Objetivo'/>
+        <Button title='Agregar'/>
+      </View>
+      <View>
+        <Text>lista de Objetivos</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 50,
+    backgroundColor: '#ffec00'
   },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  textInput:{
+    backgroundColor: 'white',
+    borderWidth: 1,
+    borderColor: 'black',
+    width: '80%',
+    marginRight: 8,
+    padding: 8
+  }
 });
